9f54b5de02e91f13df5c17d9835e6622a3b59499
8luerose <8luerose@naver.com>
2023-06-29 16:00:51 +0900

    make dot

--- test.c
+++ test.c
@@ -6,13 +6,13 @@
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 20:50:44 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/06/29 13:37:41 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/06/29 16:00:18 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 
-int	create_argb(int a, int r, int g, int b)
+int	create_trgb(int a, int r, int g, int b)
 {
 	return (a << 24 | r << 16 | g << 8 | b);
 }
@@ -23,24 +23,29 @@ int main()
 	mlx = mlx_init();
 	void *win;
 	win = mlx_new_window(mlx, 1000, 1000, "test");
-	void	*image = mlx_new_image(mlx, 500, 500);
-	int		bit_per_pixel1 = 0;
-	int		size_line1 = 0;
-	int		endian1 = 0;
-	char	*ptr = mlx_get_data_addr(image, &bit_per_pixel1, &size_line1, &endian1);
-
-	for (int i = 0; i < 200; i++)
-	{
-		for(int k = 0; k < 200; k++)
-		{
-			char *dst2;
-			dst2 = ptr + (k * size_line1 + i * bit_per_pixel1 / 8);
-			*(unsigned int *)dst2 = create_argb(0, 0, 255, 0);
-		}
-	}
-
-	mlx_put_image_to_window(mlx, win, image, 0, 0);
+	
+	//int color = create_trgb(255,255,255,255);
+	mlx_pixel_put (mlx, win, 100, 100, 0xFFFFFF);
 	mlx_loop(mlx);
+	
+	// void	*image = mlx_new_image(mlx, 500, 500);
+	// int		bit_per_pixel1 = 0;
+	// int		size_line1 = 0;
+	// int		endian1 = 0;
+	// char	*ptr = mlx_get_data_addr(image, &bit_per_pixel1, &size_line1, &endian1);
+
+	// for (int i = 0; i < 200; i++)
+	// {
+	// 	for(int k = 0; k < 200; k++)
+	// 	{
+	// 		char *dst2;
+	// 		dst2 = ptr + (k * size_line1 + i * bit_per_pixel1 / 8);
+	// 		*(unsigned int *)dst2 = create_argb(0, 0, 255, 0);
+	// 	}
+	// }
+
+	// mlx_put_image_to_window(mlx, win, image, 0, 0);
+	// mlx_loop(mlx);
 	
 	return 0;
 }
