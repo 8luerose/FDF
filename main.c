a235220e05b1550ca98b6133cfdb3b3b42b0f9b6
8luerose <8luerose@naver.com>
2023-07-02 15:27:26 +0900

    ing~ draw_line (End_parsing, mlx)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 01:36:10 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/07/02 07:17:54 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/07/02 15:25:12 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@ int	main(int ac, char **av)
 	t_vars	vars;
 	t_image	image;
 
-	if (ac != 2)
-		return (0);
-	if (!is_valid_file(av[1]))
-		ft_perror("ERROR: Invalid file name");
-	parsing(av[1], &map);
-	isometric(&map);
-	set_mlx(&vars, &image);
-	
-	print_mlx(&vars, &image);
+	if (ac == 2)
+	{
+		if (!is_valid_file(av[1]))
+			ft_perror("ERROR: Invalid file name");
+		parsing(av[1], &map);
+		isometric(&map);
+		set_mlx(&vars, &image);
+		draw
+		print_mlx(&vars, &image);
+	}
 	return (0);
 }
