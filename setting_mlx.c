21c04fb7e7acac0897b2f36f6de99832473ce7be
8luerose <8luerose@naver.com>
2023-07-05 02:21:09 +0900

    HOME_ mlx input color, print, scaling

--- setting_mlx.c
+++ setting_mlx.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   setting_mlx.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: taehkwon <taehkwon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 06:58:13 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/07/04 19:54:08 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/07/05 01:58:28 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,6 @@ void	set_mlx(t_vars *vars, t_image *image)
 	vars->win = mlx_new_window(vars->mlx, WIDTH, HEIGHT, "FDF");
 	image->img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
 	image->ptr = mlx_get_data_addr(image->img, &image->bits_per_pixel, &image->line_length, &image->endian);
-}
-
-void	print_mlx(t_vars *vars, t_image *image)
-{
-	mlx_put_image_to_window(vars->mlx, vars->win, image->img, 150, 150);
-	mlx_hook(vars->win, 02, 1L << 0, quit_window, vars);
-	mlx_loop(vars->mlx);
-}
-
-int	quit_window(int ket_code, t_vars *vars)
-{
-	if (ket_code == 53)
-	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		exit(0);
-	}
-	return (0);
 }
 
 void	draw_img(t_image *image, t_map *map)
@@ -65,4 +48,21 @@ void	pixel_input_color(t_image *image, int x, int y, int color)
 
 	dst = image->ptr + (y * image->line_length + x * (image->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+void	print_mlx(t_vars *vars, t_image *image)
+{
+	mlx_put_image_to_window(vars->mlx, vars->win, image->img, 150, 150);
+	mlx_hook(vars->win, 02, 1L << 0, quit_window, vars);
+	mlx_loop(vars->mlx);
+}
+
+int	quit_window(int ket_code, t_vars *vars)
+{
+	if (ket_code == 53)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
 }
